4fb09e040e2266fc89555aa2a9a171e8e057626d
Greensaler <medvedev@topengineer.ru>
2022-03-02 23:46:12 +0300

    Var

--- Module3_5/Program.cs
+++ Module3_5/Program.cs
@@ -6,7 +6,26 @@ namespace Module3_5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+			string MyName = "Евгения";
+			Console.WriteLine("Привет, мир");
+			Console.WriteLine();
+			Console.WriteLine("Меня зовут {0}", MyName);
+
+			//0 — позиция нашей переменной. То есть переменная будет на позиции 0.
+
+
+			//Также мы можем вставить в строку не переменную, а строковый литерал:
+			Console.WriteLine("Меня зовут {0}", "Маша");
+
+
+			//И, естественно, мы можем использовать несколько переменных при выводе. В этом случае располагаем их в порядке нумерации «мест».
+			var age = 27;
+			var weight = 50;
+			var name = "Женя";
+			var favcolor = 3;
+			Console.WriteLine("Возраст {0} \n Вес {1}", age, weight);
+			Console.WriteLine("{0} \n {1} \n {2}", name, age, favcolor);
+			Console.ReadKey();
+		}
     }
 }
